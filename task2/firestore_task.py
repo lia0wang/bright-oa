f92473a3998d869d970a23bcddbbbd8425dbef35
lia0wang <lia0wang@outlook.com>
2023-04-30 00:57:34 +1000

    task2 finished

--- task2/firestore_task.py
+++ task2/firestore_task.py
@@ -2,7 +2,6 @@ import uuid
 import firebase_admin
 from firebase_admin import credentials, firestore
 from task1.scraper import scrape_yelp_data
-import pygeohash as pgh
 
 # Use a service account
 cred = credentials.Certificate('./service_account.json')
@@ -11,11 +10,10 @@ firebase_admin.initialize_app(cred)
 # Initialize Firestore database
 db = firestore.client()
 
+
 def get(venue_data, key):
     return venue_data[key] if key in venue_data.keys() else None
 
-def calculate_geohash(lat, lon):
-    return pgh.encode(lat, lon)
 
 # Function to write venue data to Firestore
 def write_to_firestore(venue_data):
@@ -30,13 +28,14 @@ def write_to_firestore(venue_data):
         'address': get(venue_data, 'restaurant_address'),
         'price_type': get(venue_data, 'price_type'),
         'review_highlights': get(venue_data, 'review_highlights'),
-        'geohash': calculate_geohash(get(venue_data, 'latitude'), get(venue_data, 'longitude')),
+        'geohash': get(venue_data, 'geohash'),
         'latitude': get(venue_data, 'latitude'),
         'longitude': get(venue_data, 'longitude')
     }
 
     # Write the data to Firestore
     db.collection('venues').document(doc_id).set(venue_dict)
+
 
 # Call the scraping function to get venue data
 venue_data_list = scrape_yelp_data()
